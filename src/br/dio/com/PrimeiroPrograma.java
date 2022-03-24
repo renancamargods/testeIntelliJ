1d8ec6db14cc4e91c68d7bb9d209ce6bda0b6160
renancamargods <eng.renancamargodasilva@gmail.com>
2022-03-23 22:48:56 -0300

    First Commit IntelliJ

---
+++ src/br/dio/com/PrimeiroPrograma.java
@@ -0,0 +1,21 @@
+package br.dio.com;
+
+import br.dio.com.model.Gato;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+
+
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+
+  /*      int a = 5;
+        int b = 6;
+
+        System.out.println("Hello World :) " + (a+b));*/
+    }
+
+}
